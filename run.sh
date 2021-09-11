2933f046d4ad601263f9e0d034b5906c157dbf89
Guilherme Pejon <guilpejon@gmail.com>
2021-09-10 21:26:23 -0300

    Added kubernetes

---
+++ run.sh
@@ -0,0 +1,50 @@
+###############
+# DOCKER STEP #
+###############
+
+# build the image
+docker build -t guilpejon/kubernetes-rails-example:latest .
+
+# run the image
+docker run -p 3000:3000 guilpejon/kubernetes-rails-example:latest
+
+# push the image to dockerhub
+docker push guilpejon/kubernetes-rails-example:latest
+
+###################
+# KUBERNETES STEP #
+###################
+
+# download the config file from the kubernetes cluster and put it in ~/.kube/
+# then you can export KUBECONFIG or use --kubeconfig in the kubectl calls
+export KUBECONFIG=~/.kube/kubernetes-rails-kubeconfig.yaml
+
+# give the docker credentials on dockerhub to kubernetes since it is a private repo
+kubectl create secret docker-registry my-docker-secret --docker-server=DOCKER_REGISTRY_SERVER --docker-username=DOCKER_USER --docker-password=DOCKER_PASSWORD --docker-email=DOCKER_EMAIL
+kubectl edit serviceaccounts default
+# add this after "secrets:"
+# imagePullSecrets:
+# - name: my-docker-secret
+
+# apply the configuration on config/kube and creates the load balancer
+kubectl apply -f config/kube
+
+# find "LoadBalancer Ingress:" in the command bellow to learn the IP of the cluster
+kubectl describe service kubernetes-rails-load-balancer
+
+#######################
+# KUBERNETES COMMANDS #
+#######################
+
+# check if the pods are running properly
+kubectl get pods
+# get details about a pod
+kubectl describe pod pod-name
+
+# check if the services are running properly
+kubectl get services
+# get details about a service
+kubectl describe service service-name
+
+# fix configurations and regenerate the pods
+kubectl delete --all pods
